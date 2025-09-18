73ef207e7654aed23aa1dee339396c086565e5f5
Muhammad <Muhammad@DESKTOP-OJOR7F0>
2025-09-18 07:21:04 +0300

    Implement UnitOfWork with repositories and lifecycle methods
    The `UnitOfWork` class now implements the `IUnitOfWork` interface.
    It introduces dependency injection for `AppDbContext` and adds
    `Courses` and `Instructors` properties as `IGenericRepository`
    instances. A new `CompleteAsync` method is added for saving
    changes asynchronously, and a `Dispose` method ensures proper
    resource cleanup. Additional `using` directives were added to
    support the new functionality.

--- ByWay.Infrastructure/UnitOfWork/UnitOfWork.cs
+++ ByWay.Infrastructure/UnitOfWork/UnitOfWork.cs
@@ -1,6 +1,23 @@
-﻿namespace ByWay.Infrastructure.UnitOfWork
+﻿using ByWay.Application.Contracts;
+using ByWay.Domain.Entities;
+using ByWay.Infrastructure.Services.Repositories;
+
+namespace ByWay.Infrastructure.UnitOfWork
 {
-    public class UnitOfWork
+    public class UnitOfWork : IUnitOfWork
     {
+        private readonly AppDbContext _dbContext;
+        public IGenericRepository<Course> Courses { get; private set; }
+        public IGenericRepository<Instructor> Instructors { get; private set; }
+        public UnitOfWork(AppDbContext dbContext)
+        {
+            _dbContext = dbContext;
+            Courses = new GenericRepository<Course>(_dbContext);
+            Instructors = new GenericRepository<Instructor>(_dbContext);
+        }
+       
+        public async Task<int> CompleteAsync()=>await _dbContext.SaveChangesAsync();
+
+        public void Dispose() => _dbContext.Dispose();
     }
 }
