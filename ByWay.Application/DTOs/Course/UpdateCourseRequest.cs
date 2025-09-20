5f5c04c9bf88d7140febc8e32823a7fdf6921264
Muhammad <Muhammad@DESKTOP-OJOR7F0>
2025-09-20 04:14:46 +0300

    Refactor validators and update course request
    Refactored `UpdateCourseRequestValidator` and `UpdateInstructorValidator` to inherit from their respective base validators, simplifying validation logic. Added new properties to `UpdateCourseRequest` class, including `CourseName`, `PictureUrl`, `Price`, `Description`, `Rate`, `Certification`, `TotalHours`, `Level`, `Category`, and `Instructor`. Removed redundant `using` directives in affected files.

--- ByWay.Application/DTOs/Course/UpdateCourseRequest.cs
+++ ByWay.Application/DTOs/Course/UpdateCourseRequest.cs
@@ -2,5 +2,16 @@
 {
     public class UpdateCourseRequest
     {
+        public string CourseName { get; set; }
+        public string PictureUrl { get; set; }
+        public decimal Price { get; set; }
+        public string Description { get; set; }
+        public string Rate { get; set; }
+        public string Certification { get; set; }
+        public double TotalHours { get; set; }
+
+        public string Level { get; set; }
+        public string Category { get; set; }
+        public string Instructor { get; set; }
     }
 }
--- ByWay.Application/Validations/Course/UpdateCourseRequestValidator.cs
+++ ByWay.Application/Validations/Course/UpdateCourseRequestValidator.cs
@@ -1,9 +1,7 @@
-﻿using ByWay.Application.DTOs.Course;
-using FluentValidation;
-
+﻿
 namespace ByWay.Application.Validations.Course
 {
-    public class UpdateCourseRequestValidator : AbstractValidator<UpdateCourseRequest>
+    public class UpdateCourseRequestValidator : CreateCourseRequestValidator
     {
     }
 }
