e466ae6da5460372a1516d766b87130352a2b201
Muhammad <Muhammad@DESKTOP-OJOR7F0>
2025-09-30 20:10:17 +0300

    Add Cart and CartItem classes to domain entities
    Added a `Cart` class in the `ByWay.Domain.Entities.Cart` namespace to represent a shopping cart, including properties for `Id`, `UserId`, and a required collection of `CartItem` objects. The `Items` property is initialized with an empty list.
    
    Added a `CartItem` class in the same namespace to represent individual items in the cart, with properties for `Id`, `CourseName` (required), `Price`, `Quantity`, `PictureUrl` (optional), and a virtual navigation property `Course` for associated course details.

---
+++ ByWay.Domain/Entities/Cart/Cart.cs
@@ -0,0 +1,13 @@
+﻿namespace ByWay.Domain.Entities.Cart
+{
+    public class Cart
+    {
+        public int Id { get; set; }
+
+        public string UserId { get; set; } = string.Empty;
+        public required IEnumerable<CartItem> Items { get; set; }= new List<CartItem>();
+
+
+
+    }
+}
---
+++ ByWay.Domain/Entities/Cart/CartItem.cs
@@ -0,0 +1,12 @@
+﻿namespace ByWay.Domain.Entities.Cart
+{
+    public class CartItem
+    {
+        public int Id { get; set; }
+        public required string CourseName { get; set; }
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+        public string? PictureUrl { get; set; }
+        public virtual Course Course { get; set; } 
+    }
+}
