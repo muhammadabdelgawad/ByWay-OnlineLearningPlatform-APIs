9d2297c2dc9cee38c887d6c52c34b0faa7c2e112
Muhammad <Muhammad@DESKTOP-OJOR7F0>
2025-10-02 09:36:18 +0300

    Add CartController with CRUD and discount endpoints
    Introduced a new `CartController` in the `ByWay.APIs.Controllers` namespace to manage cart operations. Added endpoints for retrieving the cart, adding items, applying discounts, and clearing the cart. Integrated dependencies such as `IMapper`, `IUnitOfWork`, and `ICartService`. Included necessary `using` directives for required namespaces.

---
+++ ByWay/Controllers/CartController.cs
@@ -0,0 +1,44 @@
+﻿using AutoMapper;
+using ByWay.Application.Abstraction.Repositories.Cart;
+using ByWay.Application.Contracts;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ByWay.APIs.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CartController(IMapper mapper, IUnitOfWork unitOfWork, ICartService cartService) : ControllerBase
+    {
+
+        [HttpGet]
+        public async Task<IActionResult> GetCart()
+        {
+            var cart = await cartService.GetCartAsync(User);
+            return Ok(cart);
+        }
+
+        [HttpPost("addItem")]
+        public async Task<IActionResult> AddCartItem([FromBody] ByWay.Application.Abstraction.DTOs.Cart.CreateCartItemRequest model)
+        {
+            var cart = await cartService.AddCartItemAsync(User, model);
+            return Ok(cart);
+        }
+
+        
+
+        [HttpPost("applyDiscount")]
+        public async Task<IActionResult> ApplyDiscount([FromQuery] decimal discount)
+        {
+            await cartService.ApplyDiscountAsync(User, discount);
+            return Ok("Discount applied successfully.");
+        }
+
+        [HttpDelete("clear")]
+        public async Task<IActionResult> ClearCart()
+        {
+            await cartService.ClearCartAsync(User);
+            return Ok("Cart cleared successfully.");
+        }
+    }
+}
