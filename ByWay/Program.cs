f1e261b1de1761ed54226bfdf5a802e419adb97f
Muhammad <Muhammad@DESKTOP-OJOR7F0>
2025-10-07 17:25:13 +0300

    Enable Swagger globally and add HTTPS redirection
    Removed environment-specific condition for Swagger and Swagger UI, making them available in all environments. Added `app.UseHttpsRedirection()` to enforce HTTPS redirection, improving security.

--- ByWay/Program.cs
+++ ByWay/Program.cs
@@ -15,11 +15,10 @@ builder.Services.AddIdentityServices(builder.Configuration);
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
+
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+
 
 
 app.UseHttpsRedirection();
